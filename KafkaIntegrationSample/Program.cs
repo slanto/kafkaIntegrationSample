c9fe8faa876b4b75c9227e5d160ca89eb22ae042
slanto <slawek.antos@outlook.com>
2017-01-19 09:22:40 +0100

    Add project files.

---
+++ KafkaIntegrationSample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace KafkaIntegrationSample
+{
+    using KafkaNet;
+    using KafkaNet.Model;
+    using KafkaNet.Protocol;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new KafkaOptions(new Uri("http://localhost:9092"));
+            var router = new BrokerRouter(options);
+
+            using (var client = new Producer(router))
+            {
+                client.SendMessageAsync("test", new[] { new Message("Hi Hello! Welcome to Kafka!") }).Wait();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
